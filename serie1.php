7fc121a1b36b57fa54c2419cd97aa6cfc44c2cdc
Noodrak <Noodra.br@gmail.com>
2024-01-16 14:10:20 +0100

    Exercice 1

--- serie1.php
+++ serie1.php
@@ -1,11 +1,17 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
 <body>
- <h1>Exercice 1
- </h1>
+  <h1>Exercice 1</h1>
 <?php
-echo "My first PHP script!";
+$nom = "Bieri";
+$prenom = "Nicolas";
+echo "Bonjour $prenom $nom !"
 ?>
-
 </body>
 </html>
+
